3481ee845d16f1cba4c34b91409a5fa24a231e0d
Eddy Proca <Eddy@Eddys-MacBook-Pro-2.local>
2014-09-15 13:55:05 +0200

    html skeleton

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Lab 2 Eddy Proca</title>
+        <link rel="stylesheet" href="screen.css">
+    </head>
+    <body>
+    <?php
+
+    ?>
+    </body>
+</html>
